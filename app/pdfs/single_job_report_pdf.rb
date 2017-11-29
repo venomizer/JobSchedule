1e36c2f914b19e3ea080173918abf2e7920fa921
Julian <galindo.julian@gmail.com>
2017-11-29 14:26:57 -0600

    Updated job summary report

--- app/pdfs/single_job_report_pdf.rb
+++ app/pdfs/single_job_report_pdf.rb
@@ -2,8 +2,10 @@ class SingleJobReportPdf < Prawn::Document
   def initialize(job)
     super(page_size: [792, 1224], page_layout: :portrait)
     @job = job
+    @parts = job.parts.all
     report_title
     report_info
+    report_data
   end
 
   def report_title
@@ -20,8 +22,47 @@ class SingleJobReportPdf < Prawn::Document
   end
 
   def report_info
-    move_down 50;
-    text "<b><u>Customer:</u></b> #{@job.customer}",
+    move_down 50
+    header = ['Job Number', 'Customer', 'PO Date', 'Due Date']
+    h_info = %W(#{@job.jobNum} #{@job.customer} #{@job.poDate} #{@job.dueDate})
+    header.zip(h_info).each do |head, info|
+      text "<b><u>#{head}:</u></b> #{info}",
+           inline_format: true
+      move_down 10
+    end
+  end
+
+  def report_data
+    move_down 50
+    text '<b><u>Description:</u></b>',
          inline_format: true
+    move_down 10
+    text " - #{@job.description}"
+    move_down 50
+    text '<b><u>Parts List:</u></b>',
+         inline_format: true
+    move_down 10
+
+    if @job.parts.exists?
+      table line_items do
+            self.header = true
+            self.position = :center
+            self.column_widths = [75, 75, 250, 75, 50, 50, 75]
+            self.row_colors = %w(F0F0F0 FFFFFF)
+            self.cell_style = {align: :center, valign: :center}
+      end
+
+    else
+      text ' - No Parts'
+    end
+    move_down 50
+    text "<b><u>Status:</u></b> #{@job.status}",
+         inline_format: true
+  end
+  def line_items
+    [['Item No.', 'Part No.', 'Description', 'Quantity', 'Total Qty', 'Work Order', 'Status']] +
+        @parts.map do |part|
+          %W(#{part.item_num} #{part.partNum} #{part.description} #{part.quantity} #{part.totalQuantity} #{part.workOrder} #{part.finished ? 'Finished': 'In Progress'})
+        end
   end
 end
